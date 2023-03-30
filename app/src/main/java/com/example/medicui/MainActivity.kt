b14d079d224d51fa7bcafec4305f0cdad6997386
gilshahar7 <gilshahardex99@gmail.com>
2023-03-30 16:40:03 +0300

    Co-authored-by: Ori Mintz <orimintz21@users.noreply.github.com> Co-authored-by: orgo17 <orgo17@users.noreply.github.com> Co-authored-by: Yuval Lande <Yubkush@users.noreply.github.com> Co-authored-by: KfirYaCohen <KfirYaCohen@users.noreply.github.com>

--- app/src/main/java/com/example/medicui/MainActivity.kt
+++ app/src/main/java/com/example/medicui/MainActivity.kt
@@ -19,14 +19,31 @@ import com.example.medicui.ui.theme.MedicUITheme
 import org.json.JSONObject
 import java.time.Instant
 import java.time.Duration
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+// val channel = NotificationChannel(
+//     "channel_id",
+//     "channel_name",
+//     NotificationManager.IMPORTANCE_DEFAULT
+// ).apply {
+//     description = "channel_description"
+// }
+// val notificationManager: NotificationManager =
+//     getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+// notificationManager.createNotificationChannel(channel)
 
 //global array to store the list items, each item will be a dictionary
 var rows = mutableListOf<MutableMap<String, String>>()
 
+var rows1 = mutableListOf<MutableMap<String, String>>()
+
+//create 
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        
         // Get a reference to the AssetManager
         val assetsManager = applicationContext.assets
 
@@ -39,9 +56,19 @@ class MainActivity : ComponentActivity() {
             ""
         }
 
+        val json1 = try {
+            assetsManager.open("info.json").bufferedReader().use { it.readText() }
+        }
+        catch (e: Exception) {
+            e.printStackTrace()
+            ""
+        }
+
         // Parse the JSON data using a JSON parsing library of your choice
         // For example, using org.json
         val jsonObject = JSONObject(json)
+
+        val jsonObject1 = JSONObject(json1)
 
         //iterate over the JSON object and get the values
         val medications = jsonObject.getJSONArray("medication")
@@ -72,12 +99,25 @@ class MainActivity : ComponentActivity() {
 
                 //add the dictionary to the array
                 rows.add(row)
-            }
+            }       
         }
 
-        //sort the array by the time
-        rows.sortBy { it["time"] }
 
+        //iterate over the JSON object and get the values
+        val medications_info = jsonObject1.getJSONArray("medication_info")
+        //iterate over the medications array
+        for (i in 0 until medications_info.length()) {
+            //get the medication object
+            val medication_info = medications_info.getJSONObject(i)
+            //get the medication name
+            val name = medication_info.getString("name")
+            //get the medication general_info
+            val general_info = medication_info.getString("general_info")
+            //get the medication when_to_take
+            val when_to_take = medication_info.getString("when_to_take")
+            //get the medication side_effects
+            val side_effects = medication_info.getString("side_effects")
+        }
 
         setContent {
             MedicUITheme {
@@ -88,11 +128,60 @@ class MainActivity : ComponentActivity() {
                 ) {
                     MedApp()
                 }
+                // NavigationDrawerComposeTheme {
+                // val scaffoldState = rememberScaffoldState()
+                // val scope = rememberCoroutineScope()
+                // Scaffold(
+                //     scaffoldState = scaffoldState,
+                //     topBar = {
+                //         AppBar(
+                //             onNavigationIconClick = {
+                //                 scope.launch {
+                //                     scaffoldState.drawerState.open()
+                //                 }
+                //             }
+                //         )
+                //     },
+                //     drawerGesturesEnabled = scaffoldState.drawerState.isOpen,
+                //     drawerContent = {
+                //         DrawerHeader()
+                //         DrawerBody(
+                //             items = listOf(
+                //                 MenuItem(
+                //                     id = "home",
+                //                     title = "Home",
+                //                     contentDescription = "Go to home screen",
+                //                     icon = Icons.Default.Home
+                //                 ),
+                //                 MenuItem(
+                //                     id = "side effects",
+                //                     title = "Side Effects",
+                //                     contentDescription = "Go to side effects screen",
+                //                     icon = Icons.Default.Warning  
+                //                 ),
+                //                 MenuItem(
+                //                     id = "settings",
+                //                     title = "Settings",
+                //                     contentDescription = "Go to settings screen",
+                //                     icon = Icons.Default.Settings
+                //                 ),
+                //                 MenuItem(
+                //                     id = "help",
+                //                     title = "Help",
+                //                     contentDescription = "Get help",
+                //                     icon = Icons.Default.Info
+                //                 ),
+                //             ),
+                //             onItemClick = {
+                //                 println("Clicked on ${it.title}")
+                //             }
+                //         )
+                //     })
+                // }
             }
         }
     }
 }
-
 @Composable
 fun MedApp() {
     var expanded by remember { mutableStateOf(false) }
@@ -107,14 +196,21 @@ fun MedApp() {
         Text(text = "Medications", style = MaterialTheme.typography.h5)
         Text(text = "Today", style = MaterialTheme.typography.h6)
         for (row in rows) {
-            //q: what is the !! operator?
-            //a: https://kotlinlang.org/docs/reference/null-safety.html#the--operator
+            //format time from the ISO 8601 format string to a human readable format
+            val iso_time_string = row["time"]!!
+            //parse my_string to a YYYY-MM-DD HH:MM format
+            //split the string to get the date and time
+            val split = iso_time_string.split("T")
+            //split the date to get the year, month and day
+            val date = split[0]
+            //remove the last 4 characters from split[1]
+            var hours = split[1].substring(0, split[1].length - 4)
             
             MedCard(
                 name = row["name"]!!,
                 dosage = row["dosage"]!!,
-                //format time to a readable format from the ISO 8601 format
-                time = Instant.parse(row["time"]!!).toString(),
+                time = date + " " + hours,
+
                 expanded = expanded
             )
         }
